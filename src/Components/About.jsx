52f0af75bce9a69f3fea8c3ace6153d3dbf7587b
Rodrigo Damian Martinez <t0tti-man@hotmail.com>
2024-01-30 17:50:48 -0300

    Modifique el apartado "about"

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
             <img data-aos='fade-down' src='https://i.ibb.co/H4KDsCy/perfil2.png' width={290} height={290} className='rounded border-2 p-1 border-green-500 img_glow' alt="" />
                 <div className='h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white'>
                     <h1 data-aos='fade-right' className='text-[52px] font-semibold mb-8 leading-normal text-green-500 uppercase'>About me</h1>
-                    <p data-aos='fade-left'>How about my name is Rodrigo, I am from Buenos Aires, Argentina. I love technology and sports.</p>
+                    <p data-aos='fade-left'>I'm Rodrigo, based in Buenos Aires, Argentina. Passionate about both technology and sports, I find joy in exploring the latest innovations while staying active in the world of sports.</p>
                     <div className='flex mt-8 gap-2'>
                         <div className='flex items-center justify-center'>
                             <div className='flex space-x-2'>
